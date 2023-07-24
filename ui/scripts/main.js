6e19c65b0855af32097d28b84adaa86757a37f9d
Kristiyan Petsanov <106109643+kristiyanpts@users.noreply.github.com>
2023-07-24 11:36:44 +0300

    update folders

--- ui/scripts/main.js
+++
@@ -1,6 +0,0 @@
-let menuIcon = document.querySelector(".menu-icon");
-let navList = document.querySelector(".navlist");
-
-menuIcon.addEventListener("click", () => {
-  navList.classList.toggle("open");
-});
