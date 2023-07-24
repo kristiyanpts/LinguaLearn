62481dd1afdb3cf486400c12b9aa1f2f42053997
Kristiyan Petsanov <106109643+kristiyanpts@users.noreply.github.com>
2023-07-24 09:34:55 +0300

    update

--- client/LinguaLearn/src/app/core/constants/api.constants.ts
+++ client/LinguaLearn/src/app/core/constants/api.constants.ts
@@ -1,2 +1,2 @@
-// export const API_URL: string = 'https://lingualearn-api.onrender.com/api';
-export const API_URL: string = 'http://localhost:3000/api';
+export const API_URL: string = 'https://lingualearn-api.onrender.com/api';
+// export const API_URL: string = 'http://localhost:3000/api';
