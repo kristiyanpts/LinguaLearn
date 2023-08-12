0104e57672efbd5d33daa28d2821aa9b0ef1b670
Kristiyan Petsanov <106109643+kristiyanpts@users.noreply.github.com>
2023-08-12 20:03:14 +0300

    remove unnecessary component

--- LinguaLearn/src/app/app-routing.module.ts
+++ LinguaLearn/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/landing/home/home.component';
-import { AboutComponent } from './components/landing/about/about.component';
 import { ContactComponent } from './components/landing/contact/contact.component';
 import { NotFoundComponent } from './components/landing/not-found/not-found.component';
 import { TeachersComponent } from './components/teachers/teachers/teachers.component';
@@ -10,7 +9,6 @@ import { AdminComponent } from './components/admin-panel/admin/admin.component';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent, title: 'Home | LinguaLearn' },
-  { path: 'about', component: AboutComponent, title: 'About | LinguaLearn' },
   {
     path: 'contact',
     component: ContactComponent,
--- LinguaLearn/src/app/components/landing/landing.module.ts
+++ LinguaLearn/src/app/components/landing/landing.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { CoursesModule } from '../courses/courses.module';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    NotFoundComponent,
-    AboutComponent,
-    ContactComponent,
-  ],
+  declarations: [HomeComponent, NotFoundComponent, ContactComponent],
   imports: [
     CommonModule,
     CoursesModule,
